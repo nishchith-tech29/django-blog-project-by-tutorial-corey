a6310612d2b18a3e974376bb812e4ba66db9abd6
[Nishchith K poojary] <nishchith072@gmail.com>
2026-06-26 20:26:07 +0530

    Python Django Tutorial: Full-Featured Web App Part 7 - Login and Logout System

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -16,4 +17,6 @@ def register(request):
         form = UserRegisterForm()
     return render(request,'users/register.html',{'form':form})
 
-
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
